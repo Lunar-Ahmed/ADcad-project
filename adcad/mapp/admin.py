f4966fc28a472e42cbb9bca1acb613d247d917bf
Ahmed <lunitmohammed697@gmail.com>
2024-11-25 00:31:39 +0100

    updates

--- adcad/mapp/admin.py
+++ adcad/mapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Group
 
 # Register your models here.
+class MemberAdmin(admin.ModelAdmin):
+  list_display = ("firstname", "lastname", "joined_date",)
+  
+
+admin.site.register(Group)
--- adcad/mapp/models.py
+++ adcad/mapp/models.py
@@ -7,3 +7,6 @@ class Group(models.Model):
     phone = models.IntegerField(null=True)
     email = models.EmailField(null=True)
     joined_date = models.DateField(null=True)
+    
+    def __str__(self):
+        return f"{self.firstname} {self.lastname}"
