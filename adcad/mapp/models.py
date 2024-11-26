65ee42fcc2b37aba3fc7530313df286aaeff951a
Ahmed <lunitmohammed697@gmail.com>
2024-11-26 14:23:18 +0100

    updates

--- adcad/mapp/models.py
+++ adcad/mapp/models.py
@@ -2,12 +2,13 @@ from django.db import models
 
 
 class Group(models.Model):
-    passport = models.ImageField(upload_to='images/')
+    passport = models.ImageField(null=True,upload_to='images/')
     firstname = models.CharField(max_length=255)
     lastname = models.CharField(max_length=255)
     phone = models.IntegerField(null=True)
     email = models.EmailField(null=True)
     joined_date = models.DateField(null=True)
+    about = models.CharField(max_length=10000)
     
     def __str__(self):
         return f"{self.firstname} {self.lastname}"
--- adcad/mapp/urls.py
+++ adcad/mapp/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('group/', views.group, name='group'),
     path('group/details/<int:id>', views.details, name='details'),
     path('', views.main, name='main'),
-        path('testing/', views.testing, name='testing'),  
+    path('testing/', views.testing, name='testing'),  
 ]
