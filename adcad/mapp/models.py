544fb907638b43a316fde246a426ec8ba2066feb
Ahmed <lunitmohammed697@gmail.com>
2024-12-03 16:51:56 +0100

    updates

--- adcad/mapp/models.py
+++ adcad/mapp/models.py
@@ -8,7 +8,7 @@ class Group(models.Model):
     phone = models.IntegerField(null=True)
     email = models.EmailField(null=True)
     joined_date = models.DateField(null=True)
-    About = models.CharField(null=True, max_length=1000)
+    About = models.TextField(null=True)
     DOB = models.DateField(null=True)
     
     
