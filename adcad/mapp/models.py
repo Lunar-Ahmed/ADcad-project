e4b7e9d64bee3e34ecd212625923ffa36fd61b45
Ahmed <lunitmohammed697@gmail.com>
2024-12-04 12:21:06 +0100

    updates

--- adcad/mapp/models.py
+++ adcad/mapp/models.py
@@ -7,7 +7,8 @@ class Group(models.Model):
     lastname = models.CharField(max_length=255)
     phone = models.IntegerField(null=True)
     email = models.EmailField(null=True)
-    joined_date = models.DateField(null=True)    
+    joined_date = models.DateField(null=True)  
+    about = models.TextField(null=True)  
     
     def __str__(self):
         return f"{self.firstname} {self.lastname}"
