853073be640d8207bb9d9d7f0fafab7adec5806e
Ahmed <lunitmohammed697@gmail.com>
2024-12-09 10:27:25 +0100

    updates

--- adcad/mapp/models.py
+++ adcad/mapp/models.py
@@ -14,13 +14,13 @@ class Group(models.Model):
         return f"{self.firstname} {self.lastname}"
     
     
-from django.db import models
+# from django.db import models
 
-class InputTable(models.Model):
-    teacher = models.ForeignKey(Group, on_delete=models.CASCADE)
-    input1 = models.FloatField(default=0)
-    input2 = models.FloatField(default=0)
-    input3 = models.FloatField(default=0)
+# class InputTable(models.Model):
+#     teacher = models.ForeignKey(Group, on_delete=models.CASCADE)
+#     input1 = models.FloatField(default=0)
+#     input2 = models.FloatField(default=0)
+#     input3 = models.FloatField(default=0)
 
-    def total(self):
-        return self.input1 + self.input2 + self.input3
+#     def total(self):
+#         return self.input1 + self.input2 + self.input3
